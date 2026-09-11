5305ebb1e54e4c1e21bbb74985acc81e6ee6069d
dantelemes0 <dantelemes@gmail.com>
2026-09-10 22:29:42 -0300

    fix: corrigir url base do supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Preencha com os dados públicos do seu projeto Supabase.
 // Nunca coloque aqui a chave service_role.
 window.LOGOS_SUPABASE = {
-  url: '',
-  anonKey: ''
+  url: 'https://aqtqloxqcnlerttumnnl.supabase.co',
+  anonKey: 'sb_publishable_U1DuoJCnp5r4S5FgJ044vA_p9_Ji-hu'
 };
